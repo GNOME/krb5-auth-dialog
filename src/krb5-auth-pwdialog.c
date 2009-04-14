bd85050180403b362e529dda521915eeebca9320
Guido Günther <agx@sigxcpu.org>
2009-04-14 22:33:07 +0200

    drop "Password:" from dialog
    it's in the kerberos prompt and confusing if prompted for a PIN.

--- src/krb5-auth-pwdialog.c
+++ src/krb5-auth-pwdialog.c
@@ -195,7 +195,7 @@ ka_pwdialog_setup (KaPwDialog* pwdialog, const gchar *krb5prompt,
 	int pw4len;
 
 	if (krb5prompt == NULL) {
-		prompt = g_strdup (_("Please enter your Kerberos password."));
+		prompt = g_strdup (_("Please enter your Kerberos password:"));
 	} else {
 		/* Kerberos's prompts are a mess, and basically impossible to
 		 * translate.  There's basically no way short of doing a lot of
@@ -206,7 +206,7 @@ ka_pwdialog_setup (KaPwDialog* pwdialog, const gchar *krb5prompt,
 		pw4len = strlen ("Password for ");
 		if (strncmp (krb5prompt, "Password for ", pw4len) == 0) {
 			gchar *uid = (gchar *) (krb5prompt + pw4len);
-			prompt = g_strdup_printf (_("Please enter the password for '%s'"), uid);
+			prompt = g_strdup_printf (_("Please enter the password for '%s':"), uid);
 		} else {
 			prompt = g_strdup (krb5prompt);
 		}

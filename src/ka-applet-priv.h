c6406d4b0441e02f861a1d919529c760305a5c0c
Guido Günther <agx@sigxcpu.org>
2010-12-13 21:04:07 +0100

    Reindent

--- src/ka-applet-priv.h
+++ src/ka-applet-priv.h
@@ -33,34 +33,32 @@
 #include "ka-pwdialog.h"
 
 G_BEGIN_DECLS
-
 #define KA_NAME _("Network Authentication")
-
 /* signals emitted by KaApplet */
-typedef enum {
-  KA_SIGNAL_ACQUIRED_TGT,	/* New TGT acquired */
-  KA_SIGNAL_RENEWED_TGT,	/* TGT got renewed */
-  KA_SIGNAL_EXPIRED_TGT,	/* TGT expired or ticket cache got destroyed */
-  KA_SIGNAL_COUNT
+    typedef enum {
+    KA_SIGNAL_ACQUIRED_TGT,     /* New TGT acquired */
+    KA_SIGNAL_RENEWED_TGT,      /* TGT got renewed */
+    KA_SIGNAL_EXPIRED_TGT,      /* TGT expired or ticket cache got destroyed */
+    KA_SIGNAL_COUNT
 } KaAppletSignalNumber;
 
 /* public functions */
-gboolean ka_applet_get_show_trayicon(const KaApplet* applet);
-void ka_applet_set_tgt_renewable(KaApplet* applet, gboolean renewable);
-gboolean ka_applet_get_tgt_renewable(const KaApplet* applet);
-guint ka_applet_get_pw_prompt_secs(const KaApplet* applet);
-KaPwDialog* ka_applet_get_pwdialog(const KaApplet* applet);
-GConfClient* ka_applet_get_gconf_client(const KaApplet* applet);
-void ka_applet_signal_emit(KaApplet* applet, KaAppletSignalNumber signum,
-                           krb5_timestamp expiry);
+gboolean ka_applet_get_show_trayicon (const KaApplet *self);
+void ka_applet_set_tgt_renewable (KaApplet *self, gboolean renewable);
+gboolean ka_applet_get_tgt_renewable (const KaApplet *self);
+guint ka_applet_get_pw_prompt_secs (const KaApplet *self);
+KaPwDialog *ka_applet_get_pwdialog (const KaApplet *self);
+GConfClient *ka_applet_get_gconf_client (const KaApplet *self);
+void ka_applet_signal_emit (KaApplet *self, KaAppletSignalNumber signum,
+                            krb5_timestamp expiry);
 
 /* create the applet */
-KaApplet* ka_applet_create(void);
+KaApplet *ka_applet_create (void);
+
 /* update tooltip and icon */
-int ka_applet_update_status(KaApplet* applet, krb5_timestamp expiry);
+int ka_applet_update_status (KaApplet *self, krb5_timestamp expiry);
 
 G_END_DECLS
-
 #ifdef ENABLE_DEBUG
 #define KA_DEBUG(fmt,...) \
     g_printf ("DEBUG: %s: " fmt "\n", __func__, ##__VA_ARGS__)
@@ -68,5 +66,4 @@ G_END_DECLS
 #define KA_DEBUG(fmt,...) \
     do { } while (0)
 #endif /* !ENABLE_DEBUG */
-
 #endif

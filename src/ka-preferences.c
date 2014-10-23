5951d74397bc1367403750b96f8c85990910110b
Guido Günther <agx@sigxcpu.org>
2014-10-23 19:43:37 +0200

    Don't use deprecated GTK_STOCK_*

--- src/ka-preferences.c
+++ src/ka-preferences.c
@@ -296,8 +296,8 @@ ka_preferences_browse_certs (GtkEntry *entry)
     filechooser = gtk_file_chooser_dialog_new(_("Choose Certificate"),
                                               GTK_WINDOW (prefs.dialog),
                                               GTK_FILE_CHOOSER_ACTION_OPEN,
-                                              GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
-                                              GTK_STOCK_OPEN, GTK_RESPONSE_ACCEPT,
+                                              _("_Cancel"), GTK_RESPONSE_CANCEL,
+                                              _("_Open"), GTK_RESPONSE_ACCEPT,
                                               NULL);
 
     current = gtk_entry_get_text (entry);

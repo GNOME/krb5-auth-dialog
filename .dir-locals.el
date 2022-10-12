f20c56e921106b686e682056245c543ce2938fd4
Guido Günther <agx@sigxcpu.org>
2022-10-12 20:49:50 +0200

    Add dir-locals.el
    This makes it simpler to indent ui files

---
+++ .dir-locals.el
@@ -0,0 +1,10 @@
+(
+ (setq auto-mode-alist (cons '("\\.ui$" . nxml-mode) auto-mode-alist))
+ (nxml-mode . (
+            (indent-tabs-mode . nil)
+            ))
+ (css-mode . (
+            (css-indent-offset . 2)
+            ))
+)
+

a2617251d7ece818173f2e6118e277dfd8eb169d
Guido Günther <agx@sigxcpu.org>
2022-10-13 14:05:48 +0200

    dir-locals: Help C code indentation

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,9 @@
 (
+ (c-mode . (
+            (c-file-style . "linux")
+            (indent-tabs-mode . nil)
+            (c-basic-offset . 4)
+            ))
  (setq auto-mode-alist (cons '("\\.ui$" . nxml-mode) auto-mode-alist))
  (nxml-mode . (
             (indent-tabs-mode . nil)
